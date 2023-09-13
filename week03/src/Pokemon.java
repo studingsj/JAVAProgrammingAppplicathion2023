95977b8ae281f9f42701fb0e28db9ff63c90a359
java_sj <toobi0809@gmail.com>
2023-09-13 15:12:46 +0900

    week03 v.09

--- week03/src/Pokemon.java
+++ week03/src/Pokemon.java
@@ -1,43 +1,49 @@
 
 
-public class Pokemon {
-    // fields, 멤버 변수, 속성
-    // 캡슐화
+class Pokemon {
+    //캡슐화
+    //정적(클래스) 변수를 사용하면 공유되는 필드를 지원
     private String name;
     private int level;
     private int hp;
-    // static member variable (정적 멤버 변수, 클래스 멤버 변수)
-    //static int pokemonCount = 0; // 인스턴스(객체) 생성없이 클래스명.형태로 사용 가능
+
+    //static member variable (정적(클래스) 멤버 변수)
+    //캡슐화 시키기(private추가, get(alt+insert)추가, game sout getPokemonCount 변경)
+    //static int pokemonCount = 0; 인스터스(객체) 생성없이 클래스명. 형태로 사용 가능
+    //객체와 관련된 인스턴스 변수 사용 불가
+    //객체와 관련된 인스턴스 메서드 호출 불가
+    //객체 자신을 가리키는 this 키위드 사용 불가
+    //static final int radius 변화하지 않는 값을 사용할때
+
+
     private static int pokemonCount = 0;
 
 
-    // constructor, 생성자(특수 형태의 메서드)
-    // 리턴타입이 없으며 클래스의 이름과 같다
-    Pokemon(){
-        System.out.println("포켓몬 객체 생성(기본 생성자)!");
+    Pokemon() {
+        System.out.println("포켓몬 객체가 생성(기본 생성자)!");
         this.hp = 100;
         pokemonCount++;
     }
 
-    // constructor overloading
-    // 생성자 오버로딩
-    Pokemon(String name){
-        //this(name, 1); // 항상 생성자 첫 번째 줄에 있어야 함
-        System.out.println("포켓몬 객체 생성(매개변수 1개 생성자)!");
+    //생성자 오버로딩 (constructor)
+
+
+    Pokemon(String name) {
+        //this(name,1); this 생성자를 삭제해야지 Count 사용 가능
+        System.out.println("포켓몬 객체가 생성(매개변수 1개 생성자)!");
         this.name = name;
-        this.level = 1;
         this.hp = 100;
+        this.level = 1 ;
         pokemonCount++;
     }
-    Pokemon(String name, int level) {
-        System.out.println("포켓몬 객체 생성(매개변수 2개 생성자)!");
+
+    Pokemon(String name, int level ) {
+        System.out.println("포켓몬 객체가 생성(매개변수 2개 생성자)!");
         this.name = name;
         this.level = level;
         this.hp = 100;
         pokemonCount++;
     }
-
-    // alt+insert
 
     public static int getPokemonCount() {
         return pokemonCount;
@@ -46,15 +52,9 @@ public class Pokemon {
     public String getName() {
         return name;
     }
-    // 실행된다. 하지만 매개변수 n은 여러 의미를 가질 수 있으므로
-    // n보다는 명확한 의미의 name을 사용해야 한다
-//    public void setName(String n) {
-//        this.name = n;
-//    }
+
     public void setName(String name) {
-        //name = name;
-        // this는 실행시점의 객체 메모리 번지 주소를 의미한다.
-        this.name = name;   // this 키워드를 사용하므로써 매개변수와 멤버변수(필드)를 구분할 수 있다.
+        this.name = name;
     }
 
     public int getLevel() {
@@ -67,11 +67,10 @@ public class Pokemon {
 
     public Pokemon setLevel(int level) {
         // 리턴타입 void -> Pokemon
+        //method chaining 기법 game-27
         this.level = level;
-        return this;  // 실행 시점의 포켓몬 객체를 리턴
+        return this; //실행 시점의 포켓몬 객체를 리턴
     }
-
-
     public int getHp() {
         return hp;
     }
@@ -80,17 +79,27 @@ public class Pokemon {
         this.hp = hp;
     }
 
-    // methods, 멤버 함수, 행위
-    void evolve(Pokemon pokemon){
+    // 메소드(methode), 멤버 함수, 행위
+    //text block 활용
+    //method chaining 기법으로 연속으롤 replace 사용
+    void evolve(Pokemon pokemon) {
         pokemon.level = pokemon.level + 1;
-        System.out.println(pokemon.name + "의 레벨이 " + pokemon.level + "으로 증가!");
+        //System.out.println("\t" + pokemon.name + "의 레벨이\n" + "\t" + pokemon.level + "으로 증가!");
+        String texts = """
+                    name의 레벨이
+                    level으로 증가
+                """.replace("name", pokemon.name)
+                .replace("level",pokemon.level + "");
+        System.out.println(texts);
     }
-    void attack(){
-        System.out.println(this.name + "이(가) 광역 도발 공격을 시전합니다");
+
+    //같은 클래스에 있어서 this.name 사용가능
+    void attack() {
+        System.out.println(this.name + "이(가) 광역 도발 공격을 시전합니다.");
     }
-    // method overloading
-    // 메서드 오버로딩
-    void attack(Pokemon pokemon){
-        System.out.println(this.name + "이(가) " + pokemon.name + "에게 공격합니다");
+
+    //method overloading 공격 당하는 포켓몬 (매개변수)
+    void attack(Pokemon pokemon) {
+        System.out.println(this.name + "이(가) " + pokemon.name + "에게 공격을 시전합니다.");
     }
 }
