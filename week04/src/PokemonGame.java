3ee5edc9f9394b3246b6f2d9197fcb25345ec132
java_sj <toobi0809@gmail.com>
2023-09-20 15:29:03 +0900

    week04 v.21

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PokemonGame {
     public static void main(String[] args) {
         System.out.println("포켓몬 게임을 시작합니다.\n야생 포켓몬이 나타났습니다.");
@@ -17,6 +19,34 @@ public class PokemonGame {
             Charizard charizard = new Charizard(wings);
         } else {
             System.out.println("여긴 영원히 실행 안됩니다.");
+        }
+
+        //플레이어 포켓몬
+        Scanner s = new Scanner(System.in);
+        System.out.println("포겟몬을 고르세요. \n1) 피카쥬 2) 꼬부기 3) 리자몽 : ");
+        int menu = s.nextInt();
+        if (menu == 1) {
+            Pikachu p1 =new Pikachu(new NoFly());
+        } else if ( menu == 2) {
+            Squirtle s1 = new Squirtle(new NoFly());
+        } else if ( menu == 3) {
+            Charizard c1 = new Charizard(new Wings());
+        } else {
+            System.out.println("정상적인 값이 아닙니다.");
+        }
+
+        int menu2;
+        while (true) {
+            System.out.print("\t1) 전투 2) 도망 3) 종료 : ");
+            menu2 = s.nextInt();
+            if (menu2 == 1) {
+
+            } else if (menu2 == 2) {
+
+            } else {
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
         }
     }
 }
