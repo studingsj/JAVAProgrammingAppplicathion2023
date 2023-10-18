fa610c3ada0769b99303149ebd810b03fa99e82c
java_sj <toobi0809@gmail.com>
2023-10-18 14:31:15 +0900

    week08 v.34

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -74,6 +74,9 @@ public class PokemonGame { //week08 NullPointerException(다른 번호 선택시
         } catch (NullPointerException err ) {
             System.out.println("플레이어 객체가 생성되지 않았습니다.");
             System.out.println("에외 내용 : " + err.getMessage());
+        } catch (IndexOutOfBoundsException err) {
+            System.out.println("존재하지 않는 스킬입니다.(기술 범위를 벗어남)");
+            System.out.println("에외 내용 : " + err.getMessage());
         } catch (Exception err) {
             System.out.println("예외가 발생했습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
